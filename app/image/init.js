9dfa777e385adf30fede7b3b653707575a4f2dd9
Alex Errington <contact@alex-errington.co.uk>
2017-02-01 16:17:33 +0000

    Added google images package

--- app/image/init.js
+++ app/image/init.js
@@ -2,6 +2,10 @@ function initImage (app) {
   app.get('/', (req, res) => {
     res.send("Hello World");
   });
+
+  app.get('/image/:terms', (req, res) => {
+    res.send(req.params);
+  });
 }
 
 module.exports = initImage
